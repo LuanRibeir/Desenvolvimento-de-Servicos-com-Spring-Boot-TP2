14ca6e4ab4762f6dbe43752371a86c008079e1ab
LuanRibeir <luaneribeiroalmeida@gmail.com>
2024-05-20 19:24:58 -0300

    Add put mapping for update a product by id

--- src/main/java/com/tp2/tp2/controller/ProductController.java
+++ src/main/java/com/tp2/tp2/controller/ProductController.java
@@ -7,10 +7,14 @@ import com.tp2.tp2.model.Product;
 
 import java.util.List;
 import java.util.ArrayList;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/product")
@@ -36,5 +40,17 @@ public class ProductController {
     public Product add(@RequestBody Product product) {
         products.add(product);
         return product;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@PathVariable int id, @RequestBody Product product) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getId() == id) {
+                products.set(i, product);
+                return ResponseEntity.ok(product);
+            }
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
